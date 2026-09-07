6d7ccf973153c9712dab4f8fd6956c763361c18d
ah9742824-oss <ah9742824@gmail.com>
2026-09-08 01:13:31 +0300

    Update galleryAdminService.js

--- frontend/src/services/galleryAdminService.js
+++ frontend/src/services/galleryAdminService.js
@@ -1,5 +1,15 @@
 import { api } from './api.js';
 
-export const listGallery = () => api.get('/gallery');
-export const createGalleryImage = (payload) => api.post('/gallery', payload);
-export const deleteGalleryImage = (id) => api.del(`/gallery/${id}`);
+export const listGallery = () => api.get('/admin/gallery');
+
+export const createGalleryImage = (payload) =>
+  api.post('/gallery', payload);
+
+export const updateGalleryVisibility = (id, is_visible) =>
+  api.patch(`/gallery/${id}/visibility`, { is_visible });
+
+export const reorderGallery = (items) =>
+  api.post('/gallery/reorder', { items });
+
+export const deleteGalleryImage = (id) =>
+  api.del(`/gallery/${id}`);
